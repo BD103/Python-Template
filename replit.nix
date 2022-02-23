803f58fafd6015c4dd0bd88f154d212cca101ecd
BD103 <59022059+BD103@users.noreply.github.com>
2022-02-22 19:05:24 -0500

    feat: support binary packages in replit.nix

--- replit.nix
+++ replit.nix
@@ -2,6 +2,21 @@
     deps = [
         pkgs.python39Full
         pkgs.poetry
+
+        # Fixes error with Poetry
+        pkgs.python39Packages.pip
+
         pkgs.vim
+        pkgs.nodePackages.prettier
     ];
+
+    env = {
+        PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib
+            pkgs.zlib
+            pkgs.glib
+        ];
+
+        LANG = "en_US.UTF-8";
+    };
 }
